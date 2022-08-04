d24f6e9e2533bc85ab6cc24972f071a434b78d7c
k-martinez <keila.martinez@zemoga.com>
2022-08-04 00:06:37 -0500

    Runners addition

---
+++ Runner Baseline 1.sh
@@ -0,0 +1,1 @@
+/Users/keilamartinez/Documents/apache-jmeter-5.5/bin/jmeter -n -t "BASELINE part 1.jmx" -l JMTestResults/$(date +"%Y_%m_%d_%I_%M_%p")_testsLog_RXP_BASELINE.log -e -o JMTestResults/$(date +"%Y_%m_%d_%I_%M_%p")_RXPTestPlan
